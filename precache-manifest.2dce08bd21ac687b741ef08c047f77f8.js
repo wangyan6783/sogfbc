c8451e18c4830140972a996682676df607981b10
J.SIMPLE <wangyan6783@gmail.com>
2020-04-24 21:46:27 -0500

    Updates

---
+++ precache-manifest.2dce08bd21ac687b741ef08c047f77f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4cc38036c0bbcf639c37ce415023f04",
+    "url": "/sogfbc/index.html"
+  },
+  {
+    "revision": "94386f9e174b5fafcfa8",
+    "url": "/sogfbc/static/css/main.6f16e409.chunk.css"
+  },
+  {
+    "revision": "2fd2361170a7e1567481",
+    "url": "/sogfbc/static/js/2.4f4beb3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sogfbc/static/js/2.4f4beb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94386f9e174b5fafcfa8",
+    "url": "/sogfbc/static/js/main.290cb6b4.chunk.js"
+  },
+  {
+    "revision": "f664d21cd0c91a7a5e34",
+    "url": "/sogfbc/static/js/runtime-main.c3485690.js"
+  }
+]);
