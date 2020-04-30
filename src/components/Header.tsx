5e4a7b1249ae30133cbbecc0a8af7908c4a39bfe
J.SIMPLE <wangyan6783@gmail.com>
2020-04-29 19:39:31 -0500

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <h1>恩霖信心聖經教會</h1>
-        <h2>Shower of Grace Faith Bible Church</h2>
+      <Link to="/" className="header-link">
+        <img className="header-logo" src={Logo} />
+        <div className="header-text">
+          <h1>恩霖信心聖經教會</h1>
+          <h2>Shower of Grace Faith Bible Church</h2>
+        </div>
       </Link>
     </header>
   );
