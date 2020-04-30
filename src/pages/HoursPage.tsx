e802b4069f04e08c413684011e675f48f5fa719e
J.SIMPLE <wangyan6783@gmail.com>
2020-04-29 20:31:33 -0500

    add content

--- src/pages/HoursPage.tsx
+++ src/pages/HoursPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import locationImage from "../assets/location.png";
 
 interface Props {
   sidebarData: sidebarData;
@@ -11,6 +12,16 @@ const HoursPage: React.FC<Props> = ({ sidebarData }) => {
       <Sidebar sidebarData={sidebarData} />
       <main className="main-content">
         <h1>聚會時間</h1>
+        <h2>國語崇拜</h2>
+        <h3>主日上午 9:30</h3>
+        <h3>主日上午 11:00</h3>
+        <h2>青少年英語崇拜</h2>
+        <h3>主日上午 11:00</h3>
+        <h2>兒童主日學</h2>
+        <h3>主日上午 9:30</h3>
+        <h3>主日上午 11:00</h3>
+        <h2>公車 Bus: Q65, Q27</h2>
+        <img className="location-image" src={locationImage} />
       </main>
     </>
   );
--- src/pages/OurBeliefPage.tsx
+++ src/pages/OurBeliefPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import ourBliefData from "../data/our-belief.data.json";
 
 interface Props {
   sidebarData: sidebarData;
@@ -11,6 +12,15 @@ const OurBeliefPage: React.FC<Props> = ({ sidebarData }) => {
       <Sidebar sidebarData={sidebarData} />
       <main className="main-content">
         <h1>我們的信仰</h1>
+        <ul>
+          {ourBliefData.data.map((item) => {
+            return (
+              <li>
+                <h3>{item}</h3>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </>
   );
