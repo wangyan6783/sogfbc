efb23a0ea7babb43cdc84167a8e5720268830439
J.SIMPLE <wangyan6783@gmail.com>
2020-05-09 10:27:02 -0500

    main mod position

--- src/components/homepage/MainModCol1.tsx
+++ src/components/homepage/MainModCol1.tsx
@@ -5,7 +5,7 @@ const MainModCol1: React.FC = () => {
   return (
     <div className="main-mod-col">
       <h2 className="main-mod-col-head">影音視頻</h2>
-      <div className="main-mod-col-content">
+      <div className="main-mod-col-content main-mod-col-content-1">
         <h3>主日崇拜線上看</h3>
         <Button1 />
       </div>
