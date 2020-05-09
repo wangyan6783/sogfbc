ad5d6a40c76af9702f2cc75869959adb3676edfb
J.SIMPLE <wangyan6783@gmail.com>
2020-05-08 21:16:22 -0500

    add content

--- src/components/homepage/MainModCol1.tsx
+++ src/components/homepage/MainModCol1.tsx
@@ -4,8 +4,9 @@ import Button1 from "../Button1";
 const MainModCol1: React.FC = () => {
   return (
     <div className="main-mod-col">
-      <h2 className="main-mod-col-head">主日崇拜線上看</h2>
+      <h2 className="main-mod-col-head">影音視頻</h2>
       <div className="main-mod-col-content">
+        <h3>主日崇拜線上看</h3>
         <Button1 />
       </div>
     </div>
